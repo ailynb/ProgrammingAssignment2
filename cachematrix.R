bb89ee70aeaa9c5ddbcfd4f076fb29731ff3ae57
Ailyn <ailynbalta@gmail.com>
2017-06-05 16:19:06 +0200

    Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# CACHING THE INVERSE OF A MATRIX
+#
 
-## Write a short comment describing this function
-
+#-----------------------------------------------------------------------------------------
+# FUNCTION makeCacheMatrix
+# Creates a special "matrix", which is really a list containing a function to:
+# - set the value of the matrix
+# - get the value of the matrix
+# - set the value of the solve
+# - get the value of the solve
+#-----------------------------------------------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        # - set the value of the matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        # - set the value of the matrix
+        get <- function() x
+        # - set the value of the inverse
+        setsolve <- function(solve) m <<- solve
+        # - get the value of the inverse
+        getsolve <- function() m
+        
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
 
+#-----------------------------------------------------------------------------------------
+# FUNCTION cacheSolve
+# Return a matrix that is the solve of 'x'
+#-----------------------------------------------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # first checks to see if the solve has already been calculated
+        m <- x$getsolve()
+        # If so, it gets the solve from the cache and skips the computation
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        # Otherwise, it calculates the solve of the data and sets the value of the solve in the cache via the setsolve function
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
